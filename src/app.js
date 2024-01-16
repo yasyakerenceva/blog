f90b6da6eefbfeac0574e1b492ddd2dc1a4a40b4
Yaroslava Kerenceva <pyi.work@yandex.ru>
2024-01-16 11:57:38 +0700

    Настроить проект

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+export const App = () => {
+	return (
+		<Div>
+			<i className="fa fa-calendar"></i>
+		</Div>
+	);
+};
