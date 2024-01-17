eb4365da5ef8a8e5a6aac76bce9a7220c4a8d9a9
Yaroslava Kerenceva <pyi.work@yandex.ru>
2024-01-17 11:11:59 +0700

    Создать BFF

---
+++ src/bff/get-user.js
@@ -0,0 +1,7 @@
+import { getUsers } from "./get-users";
+
+export const getUser = async (loginToFind) => {
+	const users = await getUsers();
+
+	return users.find(({ login }) => login === loginToFind);
+};
